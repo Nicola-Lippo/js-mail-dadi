7436f6c2b41e798b5b901b2b42c2cf1876cf5cd8
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-05-03 17:32:37 +0200

    fine post correzione

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,4 +1,18 @@
 'use strict'
+//lista persone che possono accedere
+const lista = ['primo', 'secondo', 'terzo'];
+let canAccess = false
 //chiedo all' utente la sua mail
-const emailUser = prompt("inserisci la tua email per la verifica");
-console.log (emailUser);
+const user = prompt("inserisci il tuo user per la verifica").toLowerCase();
+//verifico se la mail dell'utente e presente nella lista accesso consentito
+for (let i = 0; i < lista.length; i++) {
+    if (lista[i] === user) {
+        canAccess = true;
+    }
+}
+//stampo il risultato della verifica del controllo in console
+if (canAccess === true) {
+    console.log('puoi entrare');
+} else {
+    console.log('non puoi entrare');
+}
